8f8dd46b6b05798c6260634909b69ab7db7013a4
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-07-23 18:03:26 +0200

    Generate UI

--- app.R
+++ app.R
@@ -1,1 +1,50 @@
-# App will live here
+library(shiny)
+
+ui <-
+  fluidPage(
+
+    # - Logo -
+    fluidRow(
+      align = "center",
+      tags$img(
+        src = "logo.png", width = 100,
+        style = "padding-top: 30px;"
+      )
+    ),
+
+    # - Title -
+    fluidRow(
+      align = "center",
+      tags$h1(
+        style = "padding-top: 10px; padding-bottom: 10px;",
+        "Shiny Panda"
+      )
+    ),
+
+    # - Instructions -
+    fluidRow(
+      align = "center",
+      tags$p(
+        style = "padding-bottom: 20px;",
+        "Shiny Panda allows you to generate Pandas Profiling reports online.",
+        tags$br(),
+        "Simply upload your file, and a report will automatically download when
+        ready."
+      )
+    ),
+
+    # - Upload Button -
+    fluidRow(
+      align = "center",
+      fileInput(
+        inputId = "upload",
+        label = NULL
+      )
+    )
+  )
+
+server <-
+  function(input, output, session) {
+  }
+
+shinyApp(ui, server)
